3aa8f8a52c46d40d386880d2c129082b8edfed19
henrique dantas <henrique@hotmail.ph>
2022-06-02 14:00:16 -0300

    feedback buttons

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,72 @@
+import { CloseButton } from "./CloseButton";
+import bugSvg from "../assets/bug.svg";
+import ideaSvg from "../assets/idea.svg";
+import otherSvg from "../assets/other.svg";
+import { useState } from "react";
+
+const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugSvg,
+      alt: "Imagem de um bug",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaSvg,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: otherSvg,
+      alt: "Imagem de uma pergunta",
+    },
+  },
+};
+
+type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                onClick={() => setFeedbackType(key as FeedbackType)}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                type="button"
+              >
+                <img src={value.image.source} alt={value.image.alt}></img>
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p>hello world</p>
+      )}
+      <footer className="text-xs text-neutral-400">
+        Feito com ❤️ pela{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
+}
